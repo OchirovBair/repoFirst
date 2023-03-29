c9497e8c1c6bf47b58569b1979174ac3a92000c3
bair <upvolume@mail.ru>
2023-03-29 11:51:24 +0800

    hacker messege

--- new.js
+++ new.js
@@ -1,6 +1,16 @@
-var alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
-var array = [];
-for (var i = 0; i <= 6; i++) {
-    array[i] = alphabet[Math.floor(Math.random()*alphabet.length)]
+var string = 'javascript is awesome';
+var string1 = '';
+for (var i = 0; i < string.length; i++) {
+    if (string[i] === 'a') {
+        string1 = string1 + '4';
+    } else if ( string[i] === 'e') {
+        string1 = string1 + '3';
+    } else if ( string[i] === 'i') {
+        string1 = string1 + '1';
+    } else if ( string[i] === 'o') {
+        string1 = string1 + '0';
+    } else {
+        string1 = string1 + string[i];
+    }
 }
-console.log(array.join(''));
+console.log(string1);
