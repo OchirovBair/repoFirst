259d4a710610aab869077800cae8d82af6a746c3
bair <upvolume@mail.ru>
2023-03-29 11:05:21 +0800

    change new.js

--- new.js
+++ new.js
@@ -1,5 +1,11 @@
-var owedMoney = {};
-owedMoney['jimmy'] = 3;
-owedMoney['maria'] = 5;
-owedMoney['jimmy'] += 10;
-console.log(owedMoney.jimmy);
+var family = ['Bair', 'Aiur', 'Mama', 'Papa'];
+var name = family[Math.floor(Math.random()*4)];
+if (name === 'Bair') {
+    console.log('Hello Bair!')
+} else if (name === 'Aiur') {
+    console.log('Hello Aiur!');
+} else if (name === 'Mama') {
+    console.log('Hello Mama!');
+} else if (name === 'Papa') {
+    console.log('Hello Papa!');
+}
